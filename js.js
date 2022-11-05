43ecdcc0d92c935495b0eb38f27d0e966021637a
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2022-11-06 00:15:00 +0800

    Add case change function

--- js.js
+++ js.js
@@ -1,1 +1,18 @@
 console.log("check")
+// player type rock paper or scissor
+let Playerhand = prompt("your turn") ;
+// convert typein into uppercase 
+function SensitiveChange(word) {
+    return word.toUpperCase();
+};
+
+//and return to the html
+window.onload = function(){   //to solve https://stackoverflow.com/questions/18239430/cannot-set-property-innerhtml-of-null
+document.getElementById("player").innerHTML= SensitiveChange(Playerhand) ;
+}
+
+// computer (random of r p s)
+
+// compare 
+
+//return outcome 
